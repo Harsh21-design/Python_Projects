89ec1c04797808549095165162a16f465b3c27aa
Harsh21-design <51110904133@piemr.edu.in>
2025-08-02 16:37:42 +0530

    Added a new project

---
+++ My_Projects/False_News_Generator.py
@@ -0,0 +1,48 @@
+#Importing Random Module
+import random
+
+#Creating News(Headline) Words
+subject_list=[
+    "Elon Musk",
+    "A Bus Conductor from Bombay",
+    "Shahrukh Khan",
+    "Virat Kohli",
+    "A Indore Cat",
+    "Nirmala Sithraman",
+    "A Group of Monkeys"
+]
+
+action_list = [
+    "Sings",
+    "Eats Momos",
+    "Move by train",
+    "launces",
+    "Dances ",
+    "fights"
+]
+
+places_or_things_list = [
+    "at Rajwada",
+    "in Scary Village",
+    "at Moon",
+    "in the deep Sea",
+    "for hot cup of Chai",
+    "for Plate of Poha Jalebi"
+]
+
+#Start the News/Headline Generation Loop
+while True:
+    subject = random.choice(subject_list)
+    action = random.choice(action_list)
+    place_or_thing = random.choice(places_or_things_list)
+
+    print( 5*"#"," FunDay NewsPaper ",5*"#")
+    headline = f"BREAKING NEWS: {subject} {action} {place_or_thing}."
+    print(  headline)
+
+    new_headline = input("\nDo you want to read another funny fake headline?(Yes/No): ").strip().lower()
+    if new_headline == "no":
+        break
+
+print("### Thanks For Reading Our FunDay NewsPaper ####")
+
